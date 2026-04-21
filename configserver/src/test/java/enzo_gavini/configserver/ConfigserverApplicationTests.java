6833e2da30a822f219c3526f19c940fb8adf7b3d
enzo-gavini <enzo.gavini@laplateforme.io>
2026-04-21 05:37:07 -0300

    Project for docker

---
+++ configserver/src/test/java/enzo_gavini/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package enzo_gavini.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
